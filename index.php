4e7c5693e1e2f45e13a2f30a0718eede4de58b00
Angot Nathan <nathan@Angots-MacBook-Pro.local>
2019-01-09 14:20:42 +0200

    A header file wich is included in the include folder

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "hello";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src=""></script>
+    <title>
+    <?php echo "$pageTitle";?>
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/main.css" />
+</head>
+<body>
+    <header>
+        <h1>Hope for You</h1>
+    </header>
+    
